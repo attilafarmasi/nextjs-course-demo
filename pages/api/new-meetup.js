e4547c346ea7a1c962bd3a9fbb32dad7b74344fb
Farmasi Attila <attila.farmasi@dpdgroup.com>
2022-11-08 11:18:31 +0100

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,24 @@
-// /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
-    
+    const client = await MongoClient.connect("mongodb+srv://attilafarmasi:TriMil20150629!@cluster0.w0rapkl.mongodb.net/meetups?retryWrites=true&w=majority");
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 

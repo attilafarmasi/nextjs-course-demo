fa17c863071930e75d42cabba9bcdaa0901c0029
Farmasi Attila <attila.farmasi@dpdgroup.com>
2022-11-09 13:45:59 +0100

    Adding Head Metadata

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,5 +1,7 @@
 // our-domain.com/new-meetup
 import { useRouter } from "next/router";
+import Head from 'next/head'
+import { Fragment } from "react";
 
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
@@ -22,7 +24,15 @@ function NewMeetupPage() {
     router.push("/");
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta name="description" content="Add your own meetups and create amazing networking opportunities." />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
